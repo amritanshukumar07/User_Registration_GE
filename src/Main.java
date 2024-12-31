c90007ff48d36a9e345505fef8aefee9cc9ef78d
Amritanshu Kumar <amritanshu8271@gmail.com>
2024-12-31 21:22:41 +0530

    All Emails verified

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,43 @@ UserRegistration userRegistration= new UserRegistration();
         System.out.println("Email Validation: "+ userRegistration.validateEmail("amritanshu4532@gmail.com"));
         System.out.println("Mobile Number Validation: "+userRegistration.validateNumber("789624568956"));
         System.out.println("Password validation: "+ userRegistration.validatePassword("iefnienf23_+@%$;045"));
+        // UC3: Regex for validating email
+        String emailRegex = "^[a-zA-Z0-9+-]+([._][a-zA-Z0-9]+)*@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?$";
+
+        // Email samples to validate
+        String[] emailSamples = {
+                "abc@yahoo.com",
+                "abc-100@yahoo.com",
+                "abc.100@yahoo.com",
+                "abc111@abc.com",
+                "abc-100@abc.net",
+                "abc.100@abc.com.au",
+                "abc@1.com",
+                "abc@gmail.com.com",
+                "abc+100@gmail.com",
+                // Invalid samples
+                "abc",
+                "abc@.com.my",
+                "abc123@gmail.a",
+                "abc123@.com",
+                "abc123@.com.com",
+                ".abc@abc.com",
+                "abc()*@gmail.com",
+                "abc@%*.com",
+                "abc..2002@gmail.com",
+                "abc.@gmail.com",
+                "abc@abc@gmail.com",
+                "abc@gmail.com.1a",
+                "abc@gmail.com.aa.au"
+        };
+
+        System.out.println("Validating email samples:");
+        for (String email : emailSamples) {
+            if (UserRegistration.validateInput(email, emailRegex)) {
+                System.out.println(email + " => Valid");
+            } else {
+                System.out.println(email + " => Invalid");
+            }
+        }
     }
 }
